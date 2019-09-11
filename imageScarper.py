f0e9f0a1b39f6043580e6cdb098645429e0c773e
hendry <hendryratnam@gmail.com>
2019-09-11 12:04:45 -0400

    'first'

---
+++ imageScarper.py
@@ -0,0 +1,45 @@
+import urllib.request
+import cv2
+import numpy as np
+import os
+
+def store_images_set(negative_images_link):
+    negative_image_urls = urllib.request.urlopen(negative_images_link,timeout=5).read().decode()
+
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
+    
+    pic_num = len(os.listdir('neg/')) + 1
+
+    for i in negative_image_urls.split('\n'):
+        print('Attempting to get img')
+        try:
+            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
+            print("reading image")
+            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            print("resizing image")
+            resizied_image = cv2.resize(img, (100,100))
+            print("writing image")
+            cv2.imwrite("neg/"+str(pic_num)+'.jpg',resizied_image)
+            pic_num += 1
+            print("image done")
+            print("")
+
+        except Exception as e:
+            print(str(e))
+
+def create_index_images():
+    for file_type in ['neg']:
+        for img in os.listdir(file_type):
+            if file_type == 'neg':
+                line = file_type+'/'+img+'\n'
+                with open('bg.txt','a') as f:
+                    f.write(line)
+            elif file_type == 'pos':
+                line = file_type+'/'+img+' 1 0 0 50 50\n'
+                with open('info.dat','a') as f:
+                    f.write(line)
+
+store_images_set('http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513')
+store_images_set('http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152')
+create_index_images()
